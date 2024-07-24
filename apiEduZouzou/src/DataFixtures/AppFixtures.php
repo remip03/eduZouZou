57235651b6e04a65ec82df191be0d7eca342a139
jeremy duhem <redkardss@gmail.com>
2024-07-24 16:33:08 +0200

    sa controller

--- apiEduZouzou/src/DataFixtures/AppFixtures.php
+++ apiEduZouzou/src/DataFixtures/AppFixtures.php
@@ -2,6 +2,8 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Ecole;
+use App\Entity\SuperAdmin;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,9 +11,34 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+ //creation user admin
+ $superAdmin= new SuperAdmin();
+ $superAdmin->setLastNameSA('dupont');
+ $superAdmin->setFirstNameSA('michel');
+ $superAdmin->setMailSA('superAdmin@api.com');
+ $superAdmin->setPassSA('password');
+ $superAdmin->setTelephoneSA('0123456789');
+ $superAdmin->setAdresseSA('place de la gare');
+
+
+ $manager->persist($superAdmin);
+
+
+        $listEcoles = [];
+        for ($i = 0; $i < 5; $i++) {
+            $ecole = new Ecole();
+            $ecole->setNameEc('ecole ' . $i);
+            $ecole->setAdresseEc('adresse école ' . $i);
+            $ecole->setTelephoneEc('tel école ' . $i);
+            $ecole->setMailEc('mail école ' . $i);
+           
+            $manager->persist($ecole);
+            $listEcoles[] = $ecole;
 
         $manager->flush();
     }
+
+
+}
+
 }
