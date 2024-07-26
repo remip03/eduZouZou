805c8ff6f5347ab3ce525080a402aecab1ace008
jeremy duhem <redkardss@gmail.com>
2024-07-26 14:33:27 +0200

    refresh

--- apiEZ/src/DataFixtures/AppFixtures.php
+++ apiEZ/src/DataFixtures/AppFixtures.php
@@ -3,6 +3,8 @@
 namespace App\DataFixtures;
 
 use App\Entity\Ecole;
+use App\Entity\Message;
+use App\Entity\Messagerie;
 use App\Entity\user;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
@@ -21,37 +23,70 @@ class AppFixtures extends Fixture
     {
  //creation user admin
  $user= new user();
-
- $user->setEmail('user@api.com');
-
- $user->setRoles(['ROLE_ADMIN']);
- $user->setPassword($this->userPasswordHasher->hashPassword($user, 'password'));
- 
-
  $listUser[] = $user;
+ $ecole = new Ecole();
+ $listEcoles[] = $ecole;
+ $message = new Message;
+ $listMessages[] = $message;
+ $messagerie= new Messagerie(); 
+ $listMessagerie[] = $message;
 
 
- $manager->persist($user);
 
 
-        $listEcoles = [];
-        for ($i = 0; $i < 5; $i++) {
-            $ecole = new Ecole();
-            $ecole->setNameEc('ecole ' . $i);
-            $ecole->setAdresseEc('adresse école ' . $i);
-            $ecole->setTelEc('tel école ' . $i);
-            $ecole->setMailEc('mail école ' . $i);
 
-         
-$ecole->addUser($listUser[array_rand($listUser)]);
-           
-            $manager->persist($ecole);
-            $listEcoles[] = $ecole;
+ $listMessages = [];
+
+     $message = new Message;
+     $message->setContent('contenu message ');
+     $message->setDestinataire('ecole');
+     $message->setExpediteur('user');     
+
+     $manager->persist($message);
+
+     $listMessages[] = $message;
+
+     
+     
+
+     $listMessagerie = [];
+   
+         $messagerie = new Messagerie();
+         $messagerie->setMessages(($listMessages[array_rand($listMessages)]));
+     
+         $manager->persist($messagerie);
+         $listMessagerie[] = $messagerie;
+     
+
+     $user->setEmail('user@api.com');
+     $user->setRoles(['ROLE_ADMIN']);
+     $user->setPassword($this->userPasswordHasher->hashPassword($user, 'password'));
+     $user->setFirstName('jean');
+     $user->setLastName('dupont');
+     $user->setTel('0123456789');
+     $user->setAdresse('adresse user');
+     
+    
+     $manager->persist($user);      
+
+     $listEcoles = [];
+     for ($i = 0; $i < 5; $i++) {
+         $ecole = new Ecole();
+         $ecole->setNameEc('ecole ' . $i);
+         $ecole->setAdresseEc('adresse école ' . $i);
+         $ecole->setTelEc('tel école ' . $i);
+         $ecole->setMailEc('mail école ' . $i);
+      
+     $ecole->addUser($listUser[array_rand($listUser)]);     
+     
+         $manager->persist($ecole);     
+
+     }                
+       
 
         $manager->flush();
     }
 
-
 }
 
-}
+
