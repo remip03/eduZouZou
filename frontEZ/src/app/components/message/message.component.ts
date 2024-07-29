92a6aad3cf1a2e2db08e01ba85492dd37815a89b
jeremy duhem <redkardss@gmail.com>
2024-07-29 13:01:44 +0200

    modif message html

--- frontEZ/src/app/components/message/message.component.ts
+++ frontEZ/src/app/components/message/message.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-message',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './message.component.html',
-  styleUrl: './message.component.css'
+  styleUrl: './message.component.css',
 })
 export class MessageComponent {
-
+  constructor() {}
 }
