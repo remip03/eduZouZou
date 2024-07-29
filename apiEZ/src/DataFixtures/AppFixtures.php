7b96781d9424b4a4382d15ab2cb9ef90cd182abe
Dylan Wikaryjak <wikaryjak.dylan@gmail.com>
2024-07-29 12:19:37 +0200

    Problème régler sur les packages normalement ??

--- apiEZ/src/DataFixtures/AppFixtures.php
+++ apiEZ/src/DataFixtures/AppFixtures.php
@@ -27,6 +27,86 @@ class AppFixtures extends Fixture
     public function load(ObjectManager $manager): void
     {
 
+        // Créations des écoles
+        $listEcole = [];
+        for ($i = 0; $i < 5; $i++) {
+            $ecole = new Ecole();
+            $ecole->setNameEc('Ecole ' . $i);
+            $ecole->setAdresseEc('Adresse école ' . $i);
+            $ecole->setTelEc('Tel école ' . $i);
+            $ecole->setMailEc('Mail école ' . $i);
+            $manager->persist($ecole);
+
+            $listEcole[] = $ecole;
+        }
+
+        // Créations des classes
+        for ($i = 0; $i < 20; $i++) {
+            $classe = new Classe();
+            $classe->setNameCl('Classe ' . $i);
+            $classe->setNiveauCl('Niveau ' . $i);
+            $classe->setAnneeCl(new \DateTimeImmutable());
+            $classe->setEcole($listEcole[array_rand($listEcole)]);
+            $manager->persist($classe);
+
+            $listClasse[] = $classe;
+        }
+
+        // Créations des enfants
+        for ($i = 0; $i < 100; $i++) {
+            $enfant = new Enfant();
+            $enfant->setLastNameE('Enfant ' . $i);
+            $enfant->setFirstNameE('Prénom ' . $i);
+            $enfant->setBirthDateE(new \DateTimeImmutable());
+            $enfant->setClasse($listClasse[array_rand($listClasse)]);
+            $manager->persist($enfant);
+        }
+
+        // Création des activités.
+        for ($i = 0; $i < 10; $i++) {
+            $activite = new Activite();
+            $activite->setTypeR('type ' . $i);
+            $activite->setNameR('activite n°' . $i);
+            $activite->setDescriptionR('Voici l\'activite n°' . $i);
+            $activite->setMatiereR('Mathématiques' . $i);
+            $activite->setTypeAct('quizz n°' . $i);
+            $manager->persist($activite);
+        }
+
+        // Créations des enfants
+        for ($i = 0; $i < 100; $i++) {
+            $enfant = new Enfant();
+            $enfant->setLastNameE('Enfant ' . $i);
+            $enfant->setFirstNameE('Prénom ' . $i);
+            $enfant->setBirthDateE(new \DateTimeImmutable());
+            $enfant->setClasse($listClasse[array_rand($listClasse)]);
+            $manager->persist($enfant);
+        }
+        // Créations des enfants
+        for ($i = 0; $i < 100; $i++) {
+            $enfant = new Enfant();
+            $enfant->setLastNameE('Enfant ' . $i);
+            $enfant->setFirstNameE('Prénom ' . $i);
+            $enfant->setBirthDateE(new \DateTimeImmutable());
+            $enfant->setClasse($listClasse[array_rand($listClasse)]);
+            $manager->persist($enfant);
+        }
+
+        // Création des cours.
+        for ($i = 0; $i < 10; $i++) {
+            $cours = new Cours();
+            $cours->setTypeR('type ' . $i);
+            $cours->setNameR('cours n° ' . $i);
+            $cours->setDescriptionR('Voici le cours n° ' . $i);
+            $cours->setMatiereR('Mathématiques ' . $i);
+            $cours->setDocC('doc n° ' . $i);
+            $cours->setVideoC('video n° ' . $i);
+            $cours->setRessourceSupC('ressource supplémentaire n° ' . $i);
+            $manager->persist($cours);
+        }
+
+
+
         //creation user admin
         $user = new User();
         $listUser[] = $user;
@@ -66,56 +146,10 @@ class AppFixtures extends Fixture
         $user->setTel('0123456789');
         $user->setAdresse('adresse user');
         $user->setMessagerie($listMessagerie[array_rand($listMessagerie)]);
+        $user->setEcole($listEcole[array_rand($listEcole)]);
 
 
         $manager->persist($user);
-
-        // Créations des écoles
-        $listEcole = [];
-        for ($i = 0; $i < 5; $i++) {
-            $ecole = new Ecole();
-            $ecole->setNameEc('Ecole ' . $i);
-            $ecole->setAdresseEc('Adresse école ' . $i);
-            $ecole->setTelEc('Tel école ' . $i);
-            $ecole->setMailEc('Mail école ' . $i);
-            $manager->persist($ecole);
-
-            $listEcole[] = $ecole;
-        }
-
-        // Créations des classes
-        for ($i = 0; $i < 20; $i++) {
-            $classe = new Classe();
-            $classe->setNameCl('Classe ' . $i);
-            $classe->setNiveauCl('Niveau ' . $i);
-            $classe->setAnneeCl(new \DateTimeImmutable());
-            $classe->setEcole($listEcole[array_rand($listEcole)]);
-            $manager->persist($classe);
-        }
-
-        // Création des activités.
-        for ($i = 0 ; $i < 10 ; $i++) {
-            $activite = new Activite();
-            $activite -> setTypeR('type ' . $i);
-            $activite -> setNameR('activite n°' . $i);
-            $activite -> setDescriptionR('Voici l\'activite n°' . $i);
-            $activite -> setMatiereR('Mathématiques' . $i);
-            $activite -> setTypeAct('quizz n°' . $i);
-            $manager -> persist($activite);
-        }
-
-        // Création des cours.
-        for ($i = 0 ; $i < 10 ; $i++) {
-            $cours = new Cours();
-            $cours -> setTypeR('type ' . $i);
-            $cours -> setNameR('cours n° ' . $i);
-            $cours -> setDescriptionR('Voici le cours n° ' . $i);
-            $cours -> setMatiereR('Mathématiques ' . $i);
-            $cours -> setDocC('doc n° ' . $i);
-            $cours -> setVideoC('video n° ' . $i);
-            $cours -> setRessourceSupC('ressource supplémentaire n° ' . $i);
-            $manager -> persist($cours);
-        }
 
         $manager->flush();
     }
