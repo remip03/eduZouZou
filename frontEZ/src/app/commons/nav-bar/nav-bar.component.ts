676e962bcba4120295b1da13e0205c30bd427faa
jeremy duhem <redkardss@gmail.com>
2024-07-29 13:17:20 +0200

    refresh front

--- frontEZ/src/app/commons/nav-bar/nav-bar.component.ts
+++ frontEZ/src/app/commons/nav-bar/nav-bar.component.ts
@@ -1,26 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
-<<<<<<< HEAD
-import { MessageComponent } from '../../components/message/message.component';
-=======
+
+import { MessageComponent } from '../../pages/message/message.component';
+
 import { CommonModule } from '@angular/common';
 import { Observable } from 'rxjs';
-
->>>>>>> frontDylan
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-<<<<<<< HEAD
+
   imports: [RouterLink, MessageComponent],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.css',
-})
-export class NavBarComponent {}
-=======
-  imports: [RouterLink, CommonModule],
-  templateUrl: './nav-bar.component.html',
-  styleUrl: './nav-bar.component.css'
 })
 export class NavBarComponent {
   // isLoggedIn$: Observable<boolean>; // Observable pour suivre l'état de connexion de l'utilisateur
@@ -31,11 +23,10 @@ export class NavBarComponent {
   // }
 
   // Méthode appelée lors de l'initialisation du composant
-  ngOnInit(): void { }
+  ngOnInit(): void {}
 
   // // Méthode pour déconnecter l'utilisateur
   // logout(): void {
   //   this.authService.logout(); // Appelle la méthode logout du service AuthService
   // }
 }
->>>>>>> frontDylan
