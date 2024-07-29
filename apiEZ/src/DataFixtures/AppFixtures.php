d0d262e5ac557d796538d3b9aae8642e3fcab517
jeremy duhem <redkardss@gmail.com>
2024-07-29 10:01:14 +0200

    test

--- apiEZ/src/DataFixtures/AppFixtures.php
+++ apiEZ/src/DataFixtures/AppFixtures.php
@@ -11,7 +11,7 @@ use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
-class AppFixtures extends Fixtures
+class AppFixtures extends Fixture
 {
 
     private $userPasswordHasher;
