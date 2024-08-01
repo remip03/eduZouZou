a9ad4dbad616d74abe156a4281bccbda52f320f4
jeremy duhem <redkardss@gmail.com>
2024-08-01 13:13:23 +0200

    modif

--- frontEZ/src/app/pages/login/login.component.ts
+++ frontEZ/src/app/pages/login/login.component.ts
@@ -1,12 +1,39 @@
 import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, RouterLink],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  loginForm: FormGroup = this.formB.group({
+    username: ['', [Validators.required]],
+    password: ['', [Validators.required]],
+  });
 
+  constructor(
+    private formB: FormBuilder,
+    private authService: AuthService,
+
+    private router: Router // Inject Router to navigate to home page after successful login
+  ) {}
+
+  onLogin(): void {
+    console.log(this.loginForm.value);
+    this.authService.login(this.loginForm.value).subscribe((data) => {});
+  }
+
+  get form() {
+    return this.loginForm.controls;
+  } // getter to access form controls
 }
