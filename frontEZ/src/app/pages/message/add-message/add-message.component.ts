31c34deddb131e80dc801df5f1b9d258ce21d86e
jeremy duhem <redkardss@gmail.com>
2024-08-19 13:41:11 +0200

    test

--- frontEZ/src/app/pages/message/add-message/add-message.component.ts
+++ frontEZ/src/app/pages/message/add-message/add-message.component.ts
@@ -1,6 +1,7 @@
 import { MessageComponent } from './../message.component';
 import { Component } from '@angular/core';
 import Message from '../../../models/message.models';
+import { DatePipe } from '@angular/common';
 import {
   FormBuilder,
   FormGroup,
@@ -9,7 +10,6 @@ import {
 } from '@angular/forms';
 import { MessageService } from '../../../services/message.service';
 import { AuthService } from '../../../services/auth.service';
-import { dateTimestampProvider } from 'rxjs/internal/scheduler/dateTimestampProvider';
 
 @Component({
   selector: 'app-add-message',
@@ -17,6 +17,7 @@ import { dateTimestampProvider } from 'rxjs/internal/scheduler/dateTimestampProv
   imports: [ReactiveFormsModule, MessageComponent],
   templateUrl: './add-message.component.html',
   styleUrl: './add-message.component.css',
+  providers: [DatePipe],
 })
 export class AddMessageComponent {
   messages: Message[] = []; // variable pour stocker la liste des messages
@@ -24,7 +25,10 @@ export class AddMessageComponent {
 
   // datetime pour message
   public date = new Date();
-  public formattedDate = this.date.toLocaleDateString();
+  public formattedDate = this.datePipe.transform(
+    this.date,
+    'yyyy-MM-dd HH:mm:ss'
+  );
 
   // variable pour créer un message
   createMsg: FormGroup = this.formBuilder.group({
@@ -39,14 +43,19 @@ export class AddMessageComponent {
 
   //constructeur appel du Message Service
   constructor(
+    private datePipe: DatePipe,
     private messageService: MessageService,
     private authService: AuthService,
     private formBuilder: FormBuilder
   ) {}
 
   createMessage(): void {
+    console.log(this.formattedDate);
+
     // crée le message
     this.messageService.addMessage(this.createMsg.value).subscribe();
+    this.createMsg.patchValue({ msgDate: this.formattedDate });
+    console.log(this.createMsg.value.msgDate); // to test the value in console
     // Actualise la liste des messages
     this.messageService
       .getAllMessages()
@@ -57,6 +66,12 @@ export class AddMessageComponent {
     this.role = this.authService.getRole();
     // Affiche une notification si le message est créé avec succès
     alert('Message créé avec succès!');
+  }
+  dateAuto() {
+    this.date = new Date();
+    this.formattedDate = this.date.toLocaleDateString();
+    this.createMsg.patchValue({ msgDate: this.formattedDate });
+    //console.log(this.createMsg.value.msgDate); // to test the value in console
   }
   //
   get form() {
