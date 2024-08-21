7e132299f5676075995ad86986b77e9a6bb1d1f6
Wikaryjak <wikaryjak.dylan@gmail.com>
2024-08-21 19:11:44 +0200

    début de la page profil

--- frontEZ/src/app/pages/profil/profil.component.ts
+++ frontEZ/src/app/pages/profil/profil.component.ts
@@ -2,9 +2,11 @@ import { Component } from '@angular/core';
 import Ecole from '../../models/ecole.modelt';
 import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { Router, RouterLink } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
 import { EcoleService } from '../../services/ecole.service';
+import { UserService } from '../../services/user.service';
+import User from '../../models/user.models';
 
 @Component({
   selector: 'app-profil',
@@ -16,13 +18,15 @@ import { EcoleService } from '../../services/ecole.service';
 export class ProfilComponent {
   user: FormGroup; // Déclaration du formulaire de type FormGroup
   submitted: boolean = false; // Indicateur de soumission du formulaire
+  userId?: number;
   ecoles: Ecole[] = []; // Liste des écoles
   selectedEcoleId: number | null = null; // ID de l'école sélectionnée
 
   constructor(
     private formBuilder: FormBuilder,
+    private route: ActivatedRoute,
     private router: Router,
-    private authService: AuthService,
+    private userService: UserService,
     private ecoleService: EcoleService
   ) {
     // Initialisation du formulaire avec les contrôles et leurs validateurs
@@ -66,10 +70,46 @@ export class ProfilComponent {
     );
   }
 
-  // Méthode appelée lors du changement de sélection d'une école
-  onEcoleChange(event: Event): void {
-    const selectElement = event.target as HTMLSelectElement;
-    this.selectedEcoleId = Number(selectElement.value);
-    this.user.patchValue({ ecoleId: this.selectedEcoleId });
+  ngOnInit(): void {
+    // Récupère l'ID d'une classe à partir des paramètres de la route
+    this.userId = Number(this.route.snapshot.paramMap.get('id'));
+    if (this.userId) {
+      // Si l'ID de l'utilisateur est valide, récuprère les données de la classe
+      this.userService.getUser(this.userId).subscribe((data: User) => {
+        // Prépare les données de l'utilisateur pour le formulaire
+        const userData = { ...data, ecoleId: data.ecole.id };
+        console.log('User Data:', userData); // Ajoutez un log pour vérifier les données
+        this.user.patchValue(userData); // Met à jour le formulaire avec les données de l'utilisateur
+      });
+    }
+
+    // Récupère la liste des écoles
+    this.ecoleService.getEcoles().subscribe((data: Ecole[]) => {
+      this.ecoles = data; // Met à jour la liste des écoles
+    });
+  }
+
+  onSubmit(): void {
+    this.submitted = true; // Marque le formulaire comme soumis
+    if (this.user.invalid) {
+      return; // Si le formulaire est invalide, ne pas continuer
+    }
+
+    if (this.userId) {
+      // Si l'ID de l'utilisateur est valide, met à jour l'utilisateur
+      const updatedUser = { ...this.user.value, id: this.userId };
+      this.userService.updateUser(updatedUser).subscribe({
+        next: () => {
+          alert('Utilisateur mis à jour avec succès !'); // Affiche un message de succès
+          this.router.navigate(['/profil']); // Redirige vers la liste des utilisateurs
+        },
+        error: (error) => {
+          console.error('Erreur lors de la mise à jour de l\'utilisateur', error); // Affiche une erreur en cas d'échec
+        },
+      });
+    }
+  }
+  get form() {
+    return this.user.controls; // Récupère les contrôles du formulaire
   }
 }
