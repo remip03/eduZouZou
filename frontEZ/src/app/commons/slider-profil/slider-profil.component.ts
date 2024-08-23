4168f5fc8557173451cbbdd31c9af53ef0d334c5
Dylan Wikaryjak <wikaryjak.dylan@gmail.com>
2024-08-23 11:26:44 +0200

    Menu slide pour la page profil (push avant modif qui peuvent tout casser

--- frontEZ/src/app/commons/slider-profil/slider-profil.component.ts
+++ frontEZ/src/app/commons/slider-profil/slider-profil.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-slider-profil',
@@ -8,5 +8,67 @@ import { Component } from '@angular/core';
   styleUrl: './slider-profil.component.css'
 })
 export class SliderProfilComponent {
+  currentIndex = 0;
+  totalSlides = 2; // Nombre total de slides
+  startX: number = 0;
+  endX: number = 0;
+  isDragging: boolean = false;
 
+  slide(direction: 'left' | 'right') {
+    if (direction === 'left' && this.currentIndex > 0) {
+      this.currentIndex--;
+    } else if (direction === 'right' && this.currentIndex < this.totalSlides - 1) {
+      this.currentIndex++;
+    }
+  }
+
+  getTransform() {
+    return `translateX(-${this.currentIndex * 100 / this.totalSlides}%)`; // Ajuster la transformation en fonction de la largeur des slides
+  }
+
+  @HostListener('touchstart', ['$event'])
+  onTouchStart(event: TouchEvent) {
+    this.startX = event.touches[0].clientX;
+    this.isDragging = true;
+  }
+
+  @HostListener('touchmove', ['$event'])
+  onTouchMove(event: TouchEvent) {
+    if (this.isDragging) {
+      this.endX = event.touches[0].clientX;
+    }
+  }
+
+  @HostListener('touchend', ['$event'])
+  onTouchEnd(event: TouchEvent) {
+    this.isDragging = false;
+    this.handleSwipe();
+  }
+
+  @HostListener('mousedown', ['$event'])
+  onMouseDown(event: MouseEvent) {
+    this.startX = event.clientX;
+    this.isDragging = true;
+  }
+
+  @HostListener('mousemove', ['$event'])
+  onMouseMove(event: MouseEvent) {
+    if (this.isDragging) {
+      this.endX = event.clientX;
+    }
+  }
+
+  @HostListener('mouseup', ['$event'])
+  onMouseUp(event: MouseEvent) {
+    this.isDragging = false;
+    this.handleSwipe();
+  }
+
+  handleSwipe() {
+    if (this.startX - this.endX > 50) {
+      this.slide('right');
+    } else if (this.endX - this.startX > 50) {
+      this.slide('left');
+    }
+  }
 }
--- frontEZ/src/app/pages/profil/profil.component.ts
+++ frontEZ/src/app/pages/profil/profil.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ReturnBtnComponent } from '../../commons/return-btn/return-btn.component';
+import { SliderProfilComponent } from "../../commons/slider-profil/slider-profil.component";
 
 @Component({
   selector: 'app-profil',
   standalone: true,
-  imports: [ReturnBtnComponent],
+  imports: [ReturnBtnComponent, SliderProfilComponent],
   templateUrl: './profil.component.html',
   styleUrl: './profil.component.css'
 })
