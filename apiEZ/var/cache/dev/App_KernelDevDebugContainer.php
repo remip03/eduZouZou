6b173ec3c6de68ba421e9565a93c737777e2db03
jeremy duhem <redkardss@gmail.com>
2024-08-30 11:27:17 +0200

    test

--- apiEZ/var/cache/dev/App_KernelDevDebugContainer.php
+++ apiEZ/var/cache/dev/App_KernelDevDebugContainer.php
@@ -16,7 +16,7 @@ if (!\class_exists(App_KernelDevDebugContainer::class, false)) {
 
 return new \Container0x0sBut\App_KernelDevDebugContainer([
     'container.build_hash' => '0x0sBut',
-    'container.build_id' => '50564d67',
-    'container.build_time' => 1725000636,
+    'container.build_id' => '7e3af3a5',
+    'container.build_time' => 1725008745,
     'container.runtime_mode' => \in_array(\PHP_SAPI, ['cli', 'phpdbg', 'embed'], true) ? 'web=0' : 'web=1',
 ], __DIR__.\DIRECTORY_SEPARATOR.'Container0x0sBut');
